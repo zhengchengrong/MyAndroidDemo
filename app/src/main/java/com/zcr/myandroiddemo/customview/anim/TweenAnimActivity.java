7c06dea5c26dee11f5badb4c3668696802a38341
lin9long <lin9long@163.com>
2018-02-13 15:57:10 +0800

    完成补间动画

--- app/src/main/java/com/zcr/myandroiddemo/customview/anim/TweenAnimActivity.java
+++ app/src/main/java/com/zcr/myandroiddemo/customview/anim/TweenAnimActivity.java
@@ -24,13 +24,26 @@ import butterknife.ButterKnife;
  * Created by llz on 2018/2/13.
  */
 
-public class TweenAnimActivity extends AppCompatActivity {
+public class TweenAnimActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.tv_scale)
     TextView mTvScale;
     @BindView(R.id.view_scale)
     View mViewScale;
     Animation scaleAnimation;
+    Animation alphaAnimation;
+    Animation rotateAnimation;
+    Animation translateAnimatioin;
+    Animation allAnimatioin;
+
+    @BindView(R.id.tv_alpha)
+    TextView mTvAlpha;
+    @BindView(R.id.tv_rotate)
+    TextView mTvRotate;
+    @BindView(R.id.tv_translate)
+    TextView mTvTranslate;
+    @BindView(R.id.tv_all)
+    TextView mTvAll;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,12 +51,40 @@ public class TweenAnimActivity extends AppCompatActivity {
         setContentView(R.layout.act_tween_anim);
         ButterKnife.bind(this);
         scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scaleanim);
-        mTvScale.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        alphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alphaanim);
+        rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotateanim);
+        translateAnimatioin = AnimationUtils.loadAnimation(this, R.anim.translateanimanim);
+        allAnimatioin = AnimationUtils.loadAnimation(this, R.anim.tweenall);
+        mTvScale.setOnClickListener(this);
+        mTvAlpha.setOnClickListener(this);
+        mTvRotate.setOnClickListener(this);
+        mTvTranslate.setOnClickListener(this);
+        mTvAll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_scale:
                 mViewScale.setVisibility(View.VISIBLE);
                 mViewScale.startAnimation(scaleAnimation);
-            }
-        });
+                break;
+            case R.id.tv_alpha:
+                mViewScale.setVisibility(View.VISIBLE);
+                mViewScale.startAnimation(alphaAnimation);
+                break;
+            case R.id.tv_rotate:
+                mViewScale.setVisibility(View.VISIBLE);
+                mViewScale.startAnimation(rotateAnimation);
+                break;
+            case R.id.tv_translate:
+                mViewScale.setVisibility(View.VISIBLE);
+                mViewScale.startAnimation(translateAnimatioin);
+                break;
+            case R.id.tv_all:
+                mViewScale.setVisibility(View.VISIBLE);
+                mViewScale.startAnimation(allAnimatioin);
+                break;
+        }
     }
 }
